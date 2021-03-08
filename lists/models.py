84fe0d0acef566ca3753e3b75dbc079d1a2b42e7
richardvdoost <richard@blendergrid.com>
2021-03-08 14:53:25 +0100

    Adjust model so that items are associated with different lists

--- lists/models.py
+++ lists/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 # Create your models here.
+class List(models.Model):
+    ...
+
+
 class Item(models.Model):
     text = models.TextField(default="")
+    list = models.ForeignKey(List, default=None)
