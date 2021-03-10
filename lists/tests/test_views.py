b6336f5d173c9beca4d0a0fa056361ee4d355a0e
richardvdoost <richard@blendergrid.com>
2021-03-10 15:40:55 +0100

    Refactor list view to handle new item POSTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -36,19 +36,26 @@ class ListViewTest(TestCase):
         response = self.client.get(f"/lists/{correct_list.id}/")
         self.assertEqual(response.context["list"], correct_list)
 
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
 
-class NewListTest(TestCase):
-    def test_can_save_a_post_request(self):
-        self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.client.post(f"/lists/{correct_list.id}/", data={"item_text": "A new item for an existing list"})
+
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new list item")
+        self.assertEqual(new_item.text, "A new item for an existing list")
+        self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_after_post(self):
-        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
-        new_list = List.objects.first()
-        self.assertRedirects(response, f"/lists/{new_list.id}/")
+    def test_POST_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
 
+        response = self.client.post(f"/lists/{correct_list.id}/", data={"item_text": "A new item for an existing list"})
+        self.assertRedirects(response, f"/lists/{correct_list.id}/")
+
+
+class NewListTest(TestCase):
     def test_validation_errors_are_sent_back_to_home_page_template(self):
         response = self.client.post("/lists/new", data={"item_text": ""})
         self.assertEqual(response.status_code, 200)
@@ -60,26 +67,3 @@ class NewListTest(TestCase):
         self.client.post("/lists/new", data={"item_text": ""})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
-
-
-class NewItemTest(TestCase):
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(f"/lists/{correct_list.id}/add_item", data={"item_text": "A new item for an existing list"})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new item for an existing list")
-        self.assertEqual(new_item.list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            f"/lists/{correct_list.id}/add_item", data={"item_text": "A new item for an existing list"}
-        )
-
-        self.assertRedirects(response, f"/lists/{correct_list.id}/")
