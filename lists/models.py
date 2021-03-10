6e53943502733703296685231fca55d14547982b
richardvdoost <richard@blendergrid.com>
2021-03-10 17:15:17 +0100

    Use get_absolute_url on List model to DRY urls in views

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,11 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 
 
 # Create your models here.
 class List(models.Model):
-    ...
+    def get_absolute_url(self):
+        return reverse("view_list", args=[self.id])
 
 
 class Item(models.Model):
--- lists/views.py
+++ lists/views.py
@@ -18,7 +18,8 @@ def view_list(request, list_id):
             item = Item(text=request.POST["item_text"], list=list_)
             item.full_clean()
             item.save()
-            return redirect(f"/lists/{list_.id}/")
+            return redirect(list_)
+
         except ValidationError:
             error = "You can't have an empty list item"
 
@@ -28,11 +29,14 @@ def view_list(request, list_id):
 def new_list(request):
     list_ = List.objects.create()
     item = Item(text=request.POST["item_text"], list=list_)
+
     try:
         item.full_clean()
         item.save()
+
     except ValidationError:
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, "home.html", {"error": error})
-    return redirect(f"/lists/{list_.id}/")
+
+    return redirect(list_)
