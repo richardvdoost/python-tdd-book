b0ec22a822181e99ed1c2a690acb3c9a4867fcef
richardvdoost <richard@blendergrid.com>
2021-03-11 12:30:29 +0100

    Changes up to chapter 14

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,7 @@
-from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 
 from lists.forms import ItemForm
-from lists.models import Item, List
+from lists.models import List
 
 
 # Create your views here.
@@ -16,7 +15,7 @@ def view_list(request, list_id):
     if request.method == "POST":
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST["text"], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, "list.html", {"list": list_, "form": form})
 
@@ -25,7 +24,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST["text"], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
 
     return render(request, "home.html", {"form": form})
